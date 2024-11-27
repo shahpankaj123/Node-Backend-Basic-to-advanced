d827d87323120a088c2bd5cd310f1855afed9e4e
shahpankaj123 <aaryanshah615@gmail.com>
2024-11-27 12:45:52 +0545

    mondoose orm

--- mongo-db/src/config/db.js
+++ mongo-db/src/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connect_db = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/test_db")
-    .then(() => console.log("Connected to MongoDB"))
+    .connect(process.env.DATABASE_URL)
+    .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Error connecting to MongoDB:", err));
 };
 
